2c199cf01ec2f0267afe798c50f7e791602d6b5d
takeshi odaka <38493534+Takeshi-005@users.noreply.github.com>
2019-03-07 09:21:48 +0900

    Create documentOffset.ts

---
+++ src/js/documentOffset.ts
@@ -0,0 +1,10 @@
+import { getDocumentScroll } from "./getDocumentScroll";
+
+export function documentOffset(target: HTMLElement) {
+  let offset = { x: 0, y: 0 };
+  const rect = target.getBoundingClientRect();
+  const bodyScroll = getDocumentScroll();
+  offset.y = rect.top + bodyScroll.y;
+  offset.x = rect.left + bodyScroll.x;
+  return offset;
+}
