35e7f562a2df1180c5314742db15bf69468e8a07
takeshi odaka <38493534+Takeshi-005@users.noreply.github.com>
2019-03-08 14:13:01 +0900

    Create toBoolean.ts

---
+++ src/js/toBoolean.ts
@@ -0,0 +1,10 @@
+/**
+ * @export
+ * @param {string} booleanStr
+ * @returns {boolean}
+ */
+export function toBoolean(booleanStr: string): boolean {
+  // "true"文字列と比較した結果を返す
+  // 念のため小文字化しておく
+  return booleanStr.toLowerCase() === "true";
+}
