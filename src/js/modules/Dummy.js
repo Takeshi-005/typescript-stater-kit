0e9728e3b79ba3737634d6e4bc6d8466aa6c0112
takeshi_odaka <>
2019-04-05 00:13:53 +0900

    JSのモジュール仕様を追加

---
+++ src/js/modules/Dummy.js
@@ -0,0 +1,5 @@
+export default class Dummy {
+  constructor(elem, opts) {
+    console.log(elem, opts)
+  }
+}
---
+++ src/js/modules/Dummy2.js
@@ -0,0 +1,5 @@
+export default class Dummy2 {
+  constructor(elem, opts) {
+    console.log(elem, opts)
+  }
+}
