ac5d248e4e5ced07f7dbde51f921f82b8a1bbc81
takeshi_odaka <odaka_takeshi@odakabushi-no-MacBook-Pro-4.local>
2019-04-09 00:59:27 +0900

    修正

--- src/js/modules/index.ts
+++ src/js/modules/index.ts
@@ -1,17 +1,23 @@
 // import { pascalCase } from '../utils/'
 
 /**
- * @returns {Class} New Constructor 
+ * @returns {Class} New Constructor
  */
-Array.prototype.forEach.call(document.querySelectorAll('[data-module]'), element => {
-  const keys = element.getAttribute('data-module').split(/\s+/)
-  const opts = element.getAttribute('data-options') || null
+Array.prototype.forEach.call(
+  document.querySelectorAll("[data-module]"),
+  element => {
+    const keys = element.getAttribute("data-module").split(/\s+/);
+    const opts = element.getAttribute("data-options") || null;
 
-  keys.forEach( key => {
-    const Module = require(`./${key}`).default
-    const options = opts ? (keys.length > 1) ? JSON.parse(opts)[key] : JSON.parse(opts) : {}
+    keys.forEach(key => {
+      const Module = require(`./${key}`).default;
+      const options = opts
+        ? keys.length > 1
+          ? JSON.parse(opts)[key]
+            : JSON.parse(opts)
+        : {};
 
-    if (Module !== void 0)
-      return new Module(element, options)
-  })
-})
+      if (Module !== void 0) return new Module(element, options);
+    });
+  }
+);
