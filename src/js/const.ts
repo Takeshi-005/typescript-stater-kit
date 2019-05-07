24bd9241c8445bec1dc22232802cd75967f5b215
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2019-05-07 09:05:51 +0900

    Update const.ts

---
+++ src/js/const.ts
@@ -0,0 +1,6 @@
+export const MIN_SCREEN_SIZE = 760;
+export const ANIMATE_DURATION = 350;
+export const SLICK_DURATION = 300;
+export const SCROLL_DURATION = 800;
+export const DRAWER_DURATION = 300;
+export const OPACITY_DURATION = 500;
